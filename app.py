51597eeb7dc6cf225abb73b9b40c0de3c14ebff3
Damien Metzler <dmetzler@amazon.fr>
2022-11-16 15:14:22 +0100

    ci: check for copyrights

--- app.py
+++ app.py
@@ -3,27 +3,12 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. 2021
 # SPDX-License-Identifier: Apache-2.0
 
-import os
-
 import aws_cdk as cdk
 
 from wind_farm.wind_farm_stack import WindFarmStack
 
 
 app = cdk.App()
-WindFarmStack(
-    app,
-    "MyWindFarm",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=cdk.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-)
+WindFarmStack(app, "windfarm-sample")
 
 app.synth()
