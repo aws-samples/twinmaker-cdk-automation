12cbb3592253fa7f4e8746fd3d90b223f3bd2ab6
Damien Metzler <dmetzler@amazon.fr>
2022-11-17 17:29:12 +0100

    feat: part1 add domain model

--- wind_farm/wind_farm.py
+++ wind_farm/wind_farm.py
@@ -6,4 +6,14 @@ from twinmaker_builder import TwinMakerRoot, TwinMakerObject
 
 
 class WindFarm(TwinMakerRoot):
+    def __init__(self, description: dict) -> None:
+        super().__init__(description, fields=["city"])
+
+
+class TurbineGroup(TwinMakerObject):
     pass
+
+
+class Turbine(TwinMakerObject):
+    def __init__(self, description: dict, parent=None) -> None:
+        super().__init__(description, parent=parent, fields=["device_code"])
